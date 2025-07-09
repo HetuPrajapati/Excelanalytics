ae13393685f4d7895df5dd76a812a0a1307758d6
Hetu Prajapati <hetuprajapati16120@gmail.com>
2025-07-09 18:02:23 +0530

    Update api.js

--- p-1/frontend/src/services/api.js
+++ p-1/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api"
+const API_URL = "https://excelanalytics-backend-y5qy.onrender.com/api"
 
 // Create axios instance
 const api = axios.create({
